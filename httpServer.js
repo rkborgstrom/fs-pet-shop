bae10381b036642630a2f3e6f8d28cae0ee3afbf
rkborgstrom <37957433+rkborgstrom@users.noreply.github.com>
2018-06-12 13:13:20 -0500

    Updated http

--- httpServer.js
+++ httpServer.js
@@ -1,19 +1,66 @@
 
 'use strict';
 
-const http = require('http');
-const port = process.env.PORT || 8000;
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, 'pets.json');
 
-const server = http.createServer(function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world');
+let http = require('http');
+let port = process.env.PORT || 8000;
+
+let server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && (req.url === '/pets/' || req.url === '/pets')) {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/0') {
+    fs.readFile(petsPath, 'utf8', function(err, petsData) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsData);
+      let petsJSON = JSON.stringify(pets[0]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile(petsPath, 'utf8', function(err, petsData) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsData);
+      let petsJSON = JSON.stringify(pets[1]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
 });
 
 server.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-
-
-
-module.exports = server;
