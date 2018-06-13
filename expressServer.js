a3bf5e1247c2673dc3e34646d7bffc1cf2cc80a8
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-06-13 10:10:09 -0500

    added express server

---
+++ expressServer.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, 'pets.json');
+
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 8000;
+
+app.disable('x-powered-by');
+
+app.get('/pets/', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsData) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let pets = JSON.parse(petsData);
+
+    res.send(pets);
+  });
+});
+
+app.get('/pets/0', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsData){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    let id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(petsData);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/json');
+    res.send(pets[id]);
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
+
