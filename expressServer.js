06d13104523a4bf8efeedb4d89821012eaad9d2e
Ryan Borgstrom <37957433+rkborgstrom@users.noreply.github.com>
2018-06-14 15:24:42 -0500

    Updated Express Server

--- expressServer.js
+++ expressServer.js
@@ -8,46 +8,93 @@ let express = require('express');
 let app = express();
 let port = process.env.PORT || 8000;
 
+// let morgan = require('morgan');
+let bodyParser = require('body-parser');
+
 app.disable('x-powered-by');
+// app.use(morgan('short'));
+app.use(bodyParser.json());
 
-app.get('/pets/', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsData) {
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+app.get('/pets', function (req, res) {
+    fs.readFile(petsPath, 'utf8', function (err, petsData) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
 
-    let pets = JSON.parse(petsData);
-
-    res.send(pets);
-  });
+        let pets = JSON.parse(petsData);
+        res.set('Content-Type', 'application/json');
+        res.send(pets);
+    });
 });
 
-app.get('/pets/:id', function(req, res) {
-  fs.readFile(petsPath, 'utf8', function(err, petsData){
-    if (err) {
-      console.error(err.stack);
-      return res.sendStatus(500);
-    }
+app.post('/pets', function (req, res) {
+    fs.readFile(petsPath, 'utf8', function (readErr, petsData) {
+        if (readErr) {
+            console.error(readErr.stack);
+            return res.sendStatus(500);
+        }
+        // console.log(petsData);
+        let pets = JSON.parse(petsData);
+        // console.log(pets);
+        let age = req.body.age;
+        let kind = req.body.kind;
+        let name = req.body.name;
 
-    let id = Number.parseInt(req.params.id);
-    let pets = JSON.parse(petsData);
+        let newPet = {
+            name,
+            age,
+            kind
+        };
 
-    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-      return res.sendStatus(404);
-    }
+        if (!name || !kind || !age) {
+            return res.sendStatus(400);
+        }
 
-    res.set('Content-Type', 'application/json');
-    res.send(pets[id]);
-  });
+        // console.log(pets);
+        pets.push(newPet);
+        let newPetsJSON = JSON.stringify(pets);
+
+        // console.log(newPetsJSON);
+
+        fs.writeFile(petsPath, newPetsJSON, function (writeErr) {
+            if (writeErr) {
+                console.error(writeErr.stack);
+                return res.sendStatus(500);
+            }
+
+            res.set('Content-Type', 'application/json');
+            res.send(newPet);
+        });
+    });
 });
 
-app.use(function(req, res) {
-  res.sendStatus(404);
+app.get('/pets/:id', function (req, res) {
+    fs.readFile(petsPath, 'utf8', function (err, newPetsJSON) {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
+
+        let id = Number.parseInt(req.params.id);
+        let pets = JSON.parse(newPetsJSON);
+
+        if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+            return res.sendStatus(404);
+        }
+
+        res.set('Content-Type', 'application/json/');
+        res.send(pets[id]);
+    });
 });
 
-app.listen(port, function() {
-  console.log('Listening on port', port);
+app.use(function (req, res) {
+    res.sendStatus(404);
 });
+
+app.listen(port, function () {
+    console.log('Listening on port', port);
+});
+
 
 module.exports = app;
